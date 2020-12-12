39f7567efb534792ae186505cc24f91e25f4bf12
Алексей Филаткин <afil_0206_2_2@mail.ru>
2020-12-13 04:02:51 +0700

    NumPy+Solver

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
 import matplotlib.pyplot as plt
 from solver import *
-# Задаем положение проводника
-coordinates = []
-
-print('Вводится количетсво звеньев ломаной n, затем n строк с последовательными координатами (например,'
-      ' n=5;10 10; 20,10; 20,20; 10,20; 10,25)')
-
-for i in range(int(input())):
-    s = input().split()
-    coordinates.append((int(s[0]), int(s[1])))
-
-coordinates = np.array(coordinates).T
-
-print('Magnetic field is being calculated...')
+# Определяем проводник в виде совокупности отрезков(ломаной)
+coordinates = np.array([(10,10), (20,10), (20,20), (10,20), (10,25)]).T
+# coordinates = np.array([(20, 20), (30, 20)]).T
+# coordinates = np.array([(5, 5), (10, 10), (15, 15), (20, 20), (25, 25)]).T
 # Задаём объём простарнства, в каждой точке которого будет посчитана индукция.
-x = 30
-y = 30
-bmatrix = np.zeros((x, y))
-# Непосредственно подсчёт
-for i in range(x):
-    for j in range(y):
-        bmatrix[i][j] = bfield(i, j, coordinates)
+print('Введите размеры координатной плоскости по оси x и y соответственно')
+xrange, yrange = [int(i) for i in input().split()]
+
+print('Введите мелкость разбиения(например, 0.01)')
+m = float(input())
 # Рисуем, что получилось
 fig, ax = plt.subplots()
-x = range(x)
-y = range(y)
-z = bmatrix.T
+
 # Линиями уровня функции B=B(x1,y1) удобно построить карту силовых линий(из физики известно, что силовые линии
 # магнитного поля представляются как изолинии магнитного потока).
-ax.contour(x, y, z, 35)
+ax.contour(np.arange(xrange), np.arange(yrange), bfield(xrange, yrange, m, coordinates), 50)
 plt.title('Карта силовых линий магнитного поля (видны также искажения вследствие краевых эффектов)')
 plt.show()
-
--- solver.py
+++ solver.py
@@ -7,23 +7,26 @@ I = 3000000000000
 const = I/c
 
 
-def bfield(x, y, coordinates):
+def bfield(xrange, yrange, m, coordinates):
+    b = np.zeros((xrange, yrange))
     # Определяем длины каждого участка ломаной
     wirelength = coordinates[:, 1:] - coordinates[:, :-1]
-    # Делим на мелкие кусочки
-    divider = np.arange(0, 1, 0.01)
+    # Делим на мелкие элементы
+    divider = np.arange(0, 1, m)
     w1 = np.outer(wirelength[0], divider).T + coordinates[0][:-1]
     w2 = np.outer(wirelength[1], divider).T + coordinates[1][:-1]
-    # Длины малого участка
+    # Длины элементов
     dlx = w1[1] - w1[0]
     dly = w2[1] - w2[0]
-    # Координаты центров участков
+    # Координаты центров элементов
     w1 = w1 + dlx / 2
     w2 = w2 + dly / 2
-    # Растояния от кусочка до данной точки
-    rx = x - w1
-    ry = y - w2
-    r = (rx**2 + ry**2)**0.5
-    # Собственно говоря, применяем формулу Био-Савара-Лапласа.
-    b = const*np.abs(np.sum((dlx*ry-dly*rx)/r**3))
-    return b
+    # Растояния от кусочков до данной точки
+    for x in np.arange(xrange):
+        for y in np.arange(yrange):
+            rx = x - w1
+            ry = y - w2
+            r = (rx**2 + ry**2)**0.5
+            # Собственно говоря, применяем формулу Био-Савара-Лапласа.
+            b[x][y] = const*np.abs(np.sum((dlx*ry-dly*rx)/r**3))
+    return b.T
